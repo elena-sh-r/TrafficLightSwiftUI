32b1e69e6a2af8eda903839d4b9040fbc7a96a47
Elena Sharipova <elena.sh.r@yandex.ru>
2023-06-10 11:30:29 +0300

    Initial Commit

---
+++ TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIApp.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Elena Sharipova on 10.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
